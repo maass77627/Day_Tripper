eaac4b75d6801f07c6f3c4b3db6b28d3fd2ac149
kelly_maass <kmaass77627@gmail.com>
2024-12-01 18:52:42 -0600

    new

---
+++ index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let globalData
+
+    fetch(`https://developer.nps.gov/api/v1/parks?api_key=1A1ysntfoonKKUeUWGZEkhfdQacwcXmb9kedUFy4`)
+    .then((response) => response.json())
+    .then((json) => { 
+        console.log(json)
+        console.log(json.data[0].states)
+        console.log(json.data[0].entranceFees)
+        console.log(json.data[0].operatingHours)
+        console.log(json.data[0].activities[0].name)
+        console.log(json.data[0].fullName)
+        console.log(json.data[0].images)
+        console.log(json.data[0].images[0].url)
+        console.log(json.data[0])
+        console.log(json.data)
+        globalData = json.data
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
